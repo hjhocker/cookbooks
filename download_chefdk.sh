c32df3942a3e1329f043b82da97da560fe9da2dc
Harrison <hjhocker@gmail.com>
2015-08-04 08:31:46 -0500

    Updated the download_chefdk script

--- download_chefdk.sh
+++ download_chefdk.sh
@@ -2,3 +2,4 @@
 
 echo "Downloading Chef DF for Ubuntu 12.04, 13.10, and 14.04"
 curl -O https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.6.2-1_amd64.deb
+sudo dpkg -i chefdk_0.6.2-1_amd64.deb
