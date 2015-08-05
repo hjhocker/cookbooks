5217d942522ea4028d60040f72bdf80d7acd7377
Harrison Hocker <hjhocker@gmail.com>
2015-08-04 22:44:00 -0500

    Updates shells cookbook to apply same changes to root user

--- cookbooks/shells/recipes/default.rb
+++ cookbooks/shells/recipes/default.rb
@@ -21,7 +21,20 @@ git '/home/harrison/.oh-my-zsh/' do
   action :sync
 end
 
+git '/root/.oh-my-zsh/' do
+  repository "https://github.com/robbyrussell/oh-my-zsh.git"
+  reference "master"
+  action :sync
+end
+
 template '/home/harrison/.oh-my-zsh/themes/robbyrussell.zsh-theme' do
+  source 'robbyrussell.zsh-theme.erb'
+  owner 'harrison'
+  group 'harrison'
+  mode '0644'
+end
+
+template '/root/.oh-my-zsh/themes/robbyrussell.zsh-theme' do
   source 'robbyrussell.zsh-theme.erb'
   owner 'harrison'
   group 'harrison'
@@ -32,4 +45,6 @@ execute 'change-shell-to-zsh' do
   command 'chsh -s /bin/zsh && env zsh'
 end
 
-
+execute 'change-root-to-zsh' do
+  command 'sudo -i && chsh -s /bin/zsh'
+end
