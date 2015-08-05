b301d7a777e3b236e26e16a8c64186d853e34e09
Harrison Hocker <hjhocker@gmail.com>
2015-08-04 22:45:22 -0500

    Install mplayer

--- cookbooks/utilities/recipes/default.rb
+++ cookbooks/utilities/recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-['gdebi-core', 'vim', 'curl', 'wget', 'git', 'g++', 'ruby1.9.1', 'ruby1.9.1-dev', 'rubygems1.9.1', 'irb1.9.1', 'rdoc1.9.1', 'build-essential', 'libopenssl-ruby1.9.1', 'libssl-dev', 'zlib1g-dev'].each do |pkg|  
+['mplayer', 'gdebi-core', 'vim', 'curl', 'wget', 'git', 'g++', 'ruby1.9.1', 'ruby1.9.1-dev', 'rubygems1.9.1', 'irb1.9.1', 'rdoc1.9.1', 'build-essential', 'libopenssl-ruby1.9.1', 'libssl-dev', 'zlib1g-dev'].each do |pkg|  
   package pkg do
     action :install
   end
