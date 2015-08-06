ef61acbcd3e8a4792f85def89e09a633432998c8
Harrison Hocker <hjhocker@gmail.com>
2015-08-06 14:37:15 -0500

    solo.rb fiel now finds home directory using Ruby syntax and is no longer hard coded

--- solo.rb
+++ solo.rb
@@ -1,2 +1,4 @@
-cookbook_path [ "/home/harrison/.chef/cookbooks", "/home/harrison/.berkshelf/cookbooks" ]
-json_attribs "/home/harrison/.chef/attributes.json"
+home = Dir.home
+
+cookbook_path [ "#{home}/.chef/cookbooks", "#{home}/.berkshelf/cookbooks" ]
+json_attribs "#{home}/.chef/attributes.json"
