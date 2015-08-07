80cceb1dbe7ca54ed0b5654bf395ffdb1e771fde
Harrison Hocker <hjhocker@gmail.com>
2015-08-06 20:41:07 -0500

    Added tomcat installation

---
+++ cookbooks/tomcat7/metadata.rb
@@ -0,0 +1,7 @@
+name             'tomcat7'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures tomcat7'
+long_description 'Installs/Configures tomcat7'
+version          '0.1.0'
---
+++ cookbooks/tomcat7/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: tomcat7
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'tomcat7' do
+  action :install
+end
