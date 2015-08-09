def2163c59da8f77ee82eda697a2225595d02a11
Harrison Hocker <hjhocker@gmail.com>
2015-08-08 21:05:26 -0500

    Added nginx cookbook

---
+++ cookbooks/nginx/metadata.rb
@@ -0,0 +1,7 @@
+name             'nginx'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures nginx'
+long_description 'Installs/Configures nginx'
+version          '0.1.0'
---
+++ cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: nginx
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'nginx' do
+  action :install
+end
