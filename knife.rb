581fc33badbb393b0b7c06503004dc165d5af48f
Harrison Hocker <hjhocker@gmail.com>
2015-08-27 22:22:28 -0500

    Added chef server information and resources.

---
+++ knife.rb
@@ -0,0 +1,11 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "harrisonhocker"
+client_key               "#{current_dir}/harrisonhocker.pem"
+validation_client_name   "motobecane-validator"
+validation_key           "#{current_dir}/motobecane-validator.pem"
+chef_server_url          "https://vultr.guest/organizations/motobecane"
+cookbook_path            ["#{current_dir}/cookbooks"]
