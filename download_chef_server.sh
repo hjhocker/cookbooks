772f764a4233b14678fffd1f179782f528e75195
Harrison <hjhocker@gmail.com>
2015-08-27 20:19:16 -0500

    Update download_chef_server.sh

--- download_chef_server.sh
+++ download_chef_server.sh
@@ -4,6 +4,9 @@ PLATFORM=ubuntu
 PLATFORM_VERSION=14.10
 MACHINE_ARCH=x86_64
 
+curl -L -O https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/trusty/chef-server-core_12.1.2-1_amd64.deb
+
+
 curl -L -O "http://www.chef.io/chef/download-server?p=$PLATFORM&pv=$PLATFORM_VERSION&m=$MACHINE_ARCH&v=latest&prerelease=false"
 
 exit 0
