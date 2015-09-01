fa5a5b945da0a2cb566a1d6f4330f39e08153391
Harrison Hocker <hjhocker@gmail.com>
2015-09-01 06:52:29 -0500

    Updated harrison role

--- roles/harrison.rb
+++ roles/harrison.rb
@@ -1,5 +1,4 @@
 name "person"
 description "me"
-#run_list "recipe[make_folders]", "recipe[git]", "recipe[java]", "recipe[shells]", "recipe[utilities]"
-run_list "recipe[docker_configuration]"
+run_list "recipe[make_folders]", "recipe[git]", "recipe[java]", "recipe[shells]", "recipe[utilities]", "recipe[docker_configuration]"
 default_attributes ({"home" => "/home/harrison", "foldername" => "test_harry", "folderpath" => "/tmp", "fullname" => "Harrison Hocker", "email" => "hjhocker@gmail.com", "username" => "harrison"})
